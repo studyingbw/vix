37494914ac235aa9d02abfb54209f2aa5f42cab0
bowonc <bowon78@hotmail.com>
2016-01-11 00:50:35 -0500

    add config rb for compass

---
+++ config.rb
@@ -0,0 +1,14 @@
+require 'susy'
+require 'compass'
+require 'normalize-scss'
+require 'breakpoint'
+
+project_type = :stand_alone
+output_style = :nested
+line_comments = false
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+image_dir = "imgs"
+javascripts_dir= "js"
+
