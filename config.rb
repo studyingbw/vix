6f397ecf37193a6595a9b1615b5ce4d89ff55850
bowonc <bowon78@hotmail.com>
2016-01-21 07:00:43 -0500

    Formatting css framework based on compass

--- config.rb
+++ config.rb
@@ -1,12 +1,31 @@
+require 'sass'
 require 'susy'
 require 'compass'
+require 'compass-normalize'
+require 'compass/import-once/activate'
+require 'sassy-math'
+
+# Require any additional compass plugins here.
 
 project_type = :stand_alone
-output_style = :nested
-line_comments = false
+
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-image_dir = "imgs"
-javascripts_dir= "js"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
