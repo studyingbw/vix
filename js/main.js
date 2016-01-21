808b771446386df395127b389d896efa68e922d2
bowonc <bowon78@hotmail.com>
2016-01-21 07:02:50 -0500

    new js structure

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
-(function($){
-	var ListView = Backbone.View.extend({
-			
-		}
-	);
-})((jQuery));
+/*
+var app = app || {};
+
+app = (function($){
+	var dataBefore={};
+	var dataAfter={};
+
+	//constructor
+	class DataParser{
+		constructor: function(x,y,label){
+
+		};
+		get: function(){
+
+		};
+		set: function(){
+
+		};
+		initialize: function(){
+		};
+	}
+	//prototype
+	//Calculation  
+	//DOMEvent handler
+	//
+	function initialize(){
+		parseData();
+		prepareChart();
+		prepareUI();
+	}
+	if($(document).ready){
+		console.log("testing document loading");
+		loadingImage();
+		visualizationConfig();		
+	}
+
+	return { //main flow
+	callEventHandler();
+	}
+})(jQuery));
+*/
+
